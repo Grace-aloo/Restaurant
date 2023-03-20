f240293f6fd52fdb599e130c19ca25266f69d573
grace_aloo <graceaaloo@gmail.com>
2023-03-20 10:24:50 +0300

    added validations and seed data

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,2 +1,18 @@
 class RestaurantPizzasController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    def create 
+        rp = RestaurantPizza.create!(rp_params)
+        render json: rp, status: :created
+    end
+
+
+    private 
+    def rp_params 
+        params.permit(:price,:pizza_id,:restaurant_id)
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,25 @@
 puts "WE ROLLING"
+
+20.times do
+    Pizza.create(
+      name: Faker::Food.dish,
+      ingredients: Faker::Food.ingredient
+    )
+  end
+  
+  # Create 5 restaurants
+5.times do
+    restaurant = Restaurant.create(
+      name: Faker::Restaurant.name,
+      address: Faker::Address.full_address
+    )
+ # Add random pizzas to the restaurant with random prices between 1 and 30
+ pizzas.all.sample(5).each do |pizza|
+    restaurant_pizza = RestaurantPizza.new(
+      price: rand(1..30),
+      restaurant_id: restaurant.id,
+      pizza_id: pizza.id
+    )
+    restaurant_pizza.save    
+  
 puts "OVER AND OUT"
