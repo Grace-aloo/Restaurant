cbe3ce857b7cae9ea564f948bf63609dba130c26
grace_aloo <graceaaloo@gmail.com>
2023-03-22 11:58:08 +0300

    changed message error for price

--- app/models/restaurant_pizza.rb
+++ app/models/restaurant_pizza.rb
@@ -1,5 +1,5 @@
 class RestaurantPizza < ApplicationRecord
-    validates :price, inclusion: 1..30
+    validates :price, inclusion: {in:1..30, message:"is not within 1 and 30"}
     belongs_to :restaurant
     belongs_to :pizza
 end
